1bef35c2dcb89918887655aca381aa332c2e3b71
leva-14 <lev201611@gmail.com>
2021-09-18 20:10:41 +0600

    add .env ans .env-example file

--- project_bectur/settings.py
+++ project_bectur/settings.py
@@ -22,7 +22,7 @@ environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
 
 SECRET_KEY = env('SECRET_KEY')
 
-DEBUG = True
+DEBUG = env('DEBUG')
 
 ALLOWED_HOSTS = []
 
@@ -74,24 +74,16 @@ TEMPLATES = [
 WSGI_APPLICATION = 'project_bectur.wsgi.application'
 
 
-# Database
-# https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
 
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'mymaindb',
-        'USER': 'psadmin',
-        'PASSWORD': 'password123',
-        'HOST': 'localhost',
-        'PORT': 5432,
+        'NAME': env('NAME_DB'),
+        'USER': env('USER_NAME'),
+        'PASSWORD': env('USER_PASSWORD'),
+        'HOST': env('HOST'),
+        'PORT': env('PORT'),
     }
 }
 
